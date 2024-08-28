24c26db2e3e5fdfbb347943ee93ec296824dcece
esneiderf <esneider_aflorez@soy.sena.edu.co>
2024-08-28 08:35:04 -0500

    Add files via upload

---
+++ modulo_producto.py
@@ -0,0 +1,9 @@
+from suma import funcion_suma
+from modulo_producto import funcion_producto
+
+print("calculadora")
+valor1=int(input("Ingresa el primer valor"))
+valor2=int(input("Ingresa el segundo valor"))
+
+print("El resultado  es: ", funcion_suma(valor1, valor2))
+print("El resultado  es: ", funcion_producto(valor1, valor2))
